5889205ba4e3df9a81803a303e08b51070c0d198
xiaoliu <aa2467022050@163.com>
2023-09-12 15:46:09 +0800

    test

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -16,10 +16,22 @@ public interface CategoryMapper {
      */
     List<Category> page(Category category);
 
+    /**
+     * 根据id删除分类管理
+     * @param id
+     */
     @Delete("delete from category where id = #{id}")
     void deleteById(Long id);
 
+    /**
+     * 修改分类管理信息
+     * @param category
+     */
     void update(Category category);
 
+    /**
+     * 添加分类管理信息
+     * @param category
+     */
     void add(Category category);
 }
